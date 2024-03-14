0189726f7e5af002929f6a2213e3d78a5cfdb3a4
akcaHalit <halitakca070@gmail.com>
2024-03-14 05:14:33 +0300

    I write the function for: Withdraw the amount on which address.

--- contracts/Transfers.sol
+++ contracts/Transfers.sol
@@ -6,6 +6,7 @@ contract Bank{
     mapping(address => uint) balances;
 
     // The function has to payable to send ethers, if not error is occured.
+    // If we want to ether to a function it has to be " payable ".
     function sendEthersToContract() payable external {
         balances[msg.sender] = msg.value;
     }
@@ -13,5 +14,13 @@ contract Bank{
     function showBalance() external view returns(uint){
         return balances[msg.sender];
     } 
-    
+
+    // If we want to send ethers to a address. The Address is also has to be " payable ".
+    function withdraw(address payable to,uint amount) external {
+        require(balances[msg.sender] >= amount, "insufficient funds");      // control if we have the amount or not
+        //payable(msg.sender).transfer(balances[msg.sender]);  //Parameter is already payable. it is not necessary anymore.
+        to.transfer(amount);
+        balances[msg.sender] -= amount;        // update the balance
+    }
+
 }
